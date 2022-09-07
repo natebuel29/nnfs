d93064b9510f9eeabe1dd0c1a784acad655d4616
natebuel29 <natebuel29@gmail.com>
2022-09-07 11:33:13 -0500

    add layer dropout

--- loss.py
+++ loss.py
@@ -1,7 +1,41 @@
 import numpy as np
 
+# Common loss class
+
 
 class Loss:
+
+    # Regularization loss calculation
+    def regularization_loss(self, layer):
+
+        # 0 by default
+        regularization_loss = 0
+
+        # L1 regularization - weights
+        # calculate only when factor greater than 0
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * \
+                np.sum(np.abs(layer.weights))
+
+        # L2 regularization - weights
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * \
+                np.sum(layer.weights *
+                       layer.weights)
+
+        # L1 regularization - biases
+        # calculate only when factor greater than 0
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * \
+                np.sum(np.abs(layer.biases))
+
+        # L2 regularization - biases
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * \
+                np.sum(layer.biases *
+                       layer.biases)
+
+        return regularization_loss
 
     # Calculates the data and regularization losses
     # given model output and ground truth values
@@ -15,25 +49,3 @@ class Loss:
 
         # Return loss
         return data_loss
-
-    # Regularization loss calculation
-    def regularization_loss(self, layer):
-        regularization_loss = 0
-
-        if layer.weight_regularizer_L1 > 0:
-            regularization_loss += layer.weight_regularizer_L1 * \
-                np.sum(np.abs(layer.weights))
-
-        if layer.weight_regularizer_L2 > 0:
-            regularization_loss += layer.weight_regularizer_L2 * \
-                np.sum(np.abs(layer.weights))
-
-        if layer.bias_regularizer_L1 > 0:
-            regularization_loss += layer.bias_regularizer_L1 * \
-                np.sum(np.abs(layer.weights))
-
-        if layer.bias_regularizer_L2 > 0:
-            regularization_loss += layer.bias_regularizer_L2 * \
-                np.sum(np.abs(layer.weights))
-
-        return regularization_loss
--- optimizer_workspace.py
+++ optimizer_workspace.py
@@ -11,17 +11,21 @@ from sgd_optimizer import Optimizer_SGD
 from adagrad_optimizer import Optimizer_Adagrad
 from rms_prop_optimizer import Optimizer_RMSprop
 from adam_optimizer import Optimizer_Adam
+from layer_dropout import Layer_Dropout
 
 nnfs.init()
 # Create dataset
-X, y = spiral.create_data(samples=100, classes=3)
+X, y = spiral.create_data(samples=1000, classes=3)
 
 # Create Dense layer with 2 inputs and 64 output values
-dense1 = Layer_Dense(2, 64, weight_regularizer_L2=5e-4,
-                     bias_regularizer_L2=5e-4)
+dense1 = Layer_Dense(2, 64, weight_regularizer_l2=5e-4,
+                     bias_regularizer_l2=5e-4)
 
 # Create ReLU activation function
 activation1 = Activation_ReLU()
+
+# Create dropout layer
+dropout1 = Layer_Dropout(0.1)
 
 # Create second Dense layer with 64 input features and 3 output values
 dense2 = Layer_Dense(64, 3)
@@ -30,13 +34,14 @@ dense2 = Layer_Dense(64, 3)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
 # Create Optimizer
-optimizer = Optimizer_Adam(learning_rate=.05, decay=5e-7)
+optimizer = Optimizer_Adam(learning_rate=.09, decay=5e-5)
 
 for epoch in range(10001):
     # Forward pass
     dense1.forward(X)
     activation1.forward(dense1.output)
-    dense2.forward(activation1.output)
+    dropout1.forward(activation1.output)
+    dense2.forward(dropout1.output)
     data_loss = loss_activation.forward(dense2.output, y)
     regularization_loss = loss_activation.loss.regularization_loss(
         dense1) + loss_activation.loss.regularization_loss(dense2)
@@ -54,17 +59,35 @@ for epoch in range(10001):
               f"acc: {accuracy:.3f}, " +
               f"loss: {loss:.3f}, " +
               f"data_loss: {data_loss:.3f}, " +
-              f"reg_loss: {regularization_loss:.3f}" +
-
+              f"reg_loss: {regularization_loss:.3f}, " +
               f"lr: {optimizer.current_learning_rate}")
 
     # Backward pass
     loss_activation.backward(loss_activation.output, y)
     dense2.backward(loss_activation.dinputs)
-    activation1.backward(dense2.dinputs)
+    dropout1.backward(dense2.dinputs)
+    activation1.backward(dropout1.dinputs)
     dense1.backward(activation1.dinputs)
 
     optimizer.pre_update_params()
     optimizer.update_params(dense1)
     optimizer.update_params(dense2)
     optimizer.post_update_params()
+
+# Validate the model
+
+# Create test dataset
+X_test, y_test = spiral.create_data(samples=100, classes=3)
+
+dense1.forward(X_test)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+loss = loss_activation.forward(dense2.output, y_test)
+
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y_test.shape) == 2:
+    y_test = np.argmax(y_test, axis=1)
+
+accuracy = np.mean(predictions == y_test)
+
+print(f"validation acc: {accuracy:.3f}, loss: {loss:.3f}")
